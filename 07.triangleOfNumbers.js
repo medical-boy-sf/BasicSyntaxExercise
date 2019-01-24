2a67f176dc7f03ed70b4a8386a546780f08657ab
medical-boy-sf <medical_boy_sf@yahoo.com>
2019-01-24 19:12:54 +0200

    triangleOfNumbers.js

---
+++ 07.triangleOfNumbers.js
@@ -0,0 +1,9 @@
+function triangleOfNumbers(n) {
+    for (let i = 1; i <= n; i++) {
+        let line = "";
+        for (let j = 1; j <= i; j++) {
+            line += i + " ";
+        }
+        console.log(line);
+    }
+}
